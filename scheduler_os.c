a89d2e4c132eee2dc98696142a337937e0c7560a
Benjamin Hanbaly <bhanbaly@ttu.edu>
2026-04-26 16:19:16 -0500

    added Elevator struct and parses .bldg file into an array of Elevator structs

--- scheduler_os.c
+++ scheduler_os.c
@@ -2,25 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
+#include <pthread.h>
 
-//function to read and print the .bldg file
-void read_bldg(const char *filename) {
+// struct to hold elevator information
+typedef struct {
+    char name[10];
+    int lowest_floor;
+    int highest_floor;
+    int current_floor;
+    int capacity;
+} Elevator;
+
+//parse the .bldg file and put each elevator in the array of elevators, return # of elevators parsed
+int parse_bldg_file(const char *filename, Elevator *elevators) 
+{
     FILE *file = fopen(filename, "r");
-    if (!file) 
-    {
-        printf("Error opening .bldg file");
-        exit(1);
+    if (!file) {
+        printf("Error opening .bldg file\n");
+        return -1;
     }
 
-    printf("Elevator Configurations\n");
+    int count = 0;
     char line[256];
-    while (fgets(line, sizeof(line), file)) {
-        printf("%s", line);
-    }
-    printf("\n");
-    fclose(file);
-}
 
+    // read file
+    while (fgets(line, sizeof(line), file))
+    {
+        //extract fields
+        if (sscanf(line, "%[^\t]\t%d\t%d\t%d\t%d", elevators[count].name, &elevators[count].lowest_floor, &elevators[count].highest_floor, &elevators[count].current_floor, &elevators[count].capacity) == 5) 
+        // the above line reads a string, skips a tab, and then reads four integers separated by tabs
+        {
+            count++;
+        } 
+    }
+
+    fclose(file);
+    return count;
+}
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         printf("Error, 2 arguments required: bldg_file and port\n");
@@ -28,11 +46,13 @@ int main(int argc, char *argv[]) {
     }
 
     char *bldg_file = argv[1];
-    port = atoi(argv[2]);
+    int port = atoi(argv[2]);
+
+    Elevator building_elevators[10];
+    int num_elevators = parse_bldg_file(bldg_file, building_elevators);
+    if (num_elevators == -1) exit(1);
 
     curl_global_init(CURL_GLOBAL_ALL);
-
-    read_bldg(bldg_file);
 
     //rest of code
 
